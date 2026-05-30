85ccd250d2e1771de418d51b6bb87c00debe210a
darshan-77777 <darshanshahapure077@gmail.com>
2026-05-30 14:03:54 +0530

    Added image preview functionality

--- static/script.js
+++ static/script.js
@@ -1,3 +1,21 @@
-console.log(
-  "Emotion Music Player Loaded"
-);
+const imageInput =
+  document.getElementById("imageInput");
+
+const previewImage =
+  document.getElementById("previewImage");
+
+imageInput.addEventListener("change", function(event){
+
+  const selectedFile =
+    event.target.files[0];
+
+  if(selectedFile){
+
+    previewImage.src =
+      URL.createObjectURL(selectedFile);
+
+    previewImage.style.display =
+      "block";
+  }
+
+});
